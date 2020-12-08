477298e1515da76c4d605590f0f186ad5a52690b
Guada M <guada.m@gmail.com>
2020-12-08 15:48:10 -0300

    Add files via upload

--- src/main/java/simulador/Estadisticas.java
+++ src/main/java/simulador/Estadisticas.java
@@ -13,13 +13,16 @@ public class Estadisticas {
 
 	public static void calcularEstadisticas(float tiempoEsperaCola, float tiempoTransito, float tiempoOcioso,
 			float tiempoFinSimulacion, int cantidadItems) {
-		System.out.println("\n----------------------------------------------------------\n" 
-						 + "||                     Estadísticas                     ||\n"
-						 + "----------------------------------------------------------\n");
+		System.out.println("\n----------------------------------------------------------\n"
+				+ "||                     Estadísticas                     ||\n"
+				+ "----------------------------------------------------------\n");
+		System.out.println("> Tiempo total de espera por el descenso: " + decimal.format(tiempoEsperaCola) + " mins");
 		System.out.println("> Tiempo medio de espera por el descenso: "
-				+ decimal.format((tiempoEsperaCola / cantidadItems)) + " mins");
+				+ decimal.format((tiempoEsperaCola / cantidadItems)) + " mins\n");
+		System.out.println("> Tiempo ocioso total de la pista: " + decimal.format(tiempoOcioso) + " mins");
 		System.out.println("> Porcentaje de tiempo ocioso de la pista: "
-				+ decimal.format(((tiempoOcioso * 100) / tiempoFinSimulacion)) + " %");
+				+ decimal.format(((tiempoOcioso * 100) / tiempoFinSimulacion)) + " %\n");
+		System.out.println("> Tiempo total de tránsito: " + decimal.format(tiempoTransito) + " mins");
 		System.out.println(
 				"> Tiempo medio de tránsito: " + decimal.format((tiempoTransito / cantidadItems)) + " mins\n\n");
 
